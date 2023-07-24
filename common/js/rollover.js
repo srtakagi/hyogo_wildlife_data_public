c46dddcee5b4fe9a1e7b599ac48eb3703d28b696
srtakagi <25660332+srtakagi@users.noreply.github.com>
2023-07-24 09:54:57 +0900

    initial commit

---
+++ common/js/rollover.js
@@ -0,0 +1,36 @@
+﻿function smartRollover() {
+	if(document.getElementsByTagName) {
+		
+		var anchors = document.getElementsByTagName("a");
+		
+		for(var i=0; i < anchors.length; i++) {
+			
+			anchors[i].onmouseover = function() {
+				var images = this.getElementsByTagName("img");
+				for(var j=0; j < images.length; j++) {
+					if(images[j].getAttribute("src").match("_out."))
+					{
+						images[j].setAttribute("src", images[j].getAttribute("src").replace("_out.", "_over."));
+					}
+				}
+			}
+			anchors[i].onmouseout = function() {
+				var images = this.getElementsByTagName("img");
+				for(var j=0; j < images.length; j++) {
+					if(images[j].getAttribute("src").match("_over."))
+					{
+						images[j].setAttribute("src", images[j].getAttribute("src").replace("_over.", "_out."));
+					}
+				}
+			}
+		}
+		
+	}
+}
+
+if(window.addEventListener) {
+	window.addEventListener("load", smartRollover, false);
+}
+else if(window.attachEvent) {
+	window.attachEvent("onload", smartRollover);
+}
